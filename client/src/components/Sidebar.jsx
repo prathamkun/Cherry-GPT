74f705dcdf5a50b4c81d805d81abf82850041aa7
prathameshborkar <prathamkun@users.noreply.github.com>
2026-01-09 20:04:59 +0530

    chats addition

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -43,8 +43,24 @@ const Sidebar = () => {
       </div>
       {/* Recent Chats */}
       {chats.length > 0 && <p className='mt-4 text-sm'>Recent Chats</p>}
-      <div>
-        
+      <div className='flex-1 overflow-y-scroll mt-3 text-sm space-y-3'>
+        {
+             chats.filter((chat)=> chat.messages[0] ? chat.messages[0]?.content.toLowerCase().includes(search.toLowerCase()) : chat.name.toLowerCase().includes(search.toLowerCase())).map((chat)=>(
+                <div key={chat._id} className='p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group'>
+                    <div>
+                        <p className='truncate w-full'>
+                            {chat.messages.length > 0 ? chat.messages[0].content.slice(0,32) : chat.name}
+                        </p>
+                        <p className='text-xs text-gray-500 dark:text-[#B1A6C0]'>
+                            {chat.updatedAt}
+                        </p>
+                    </div>
+                    <img src={assets.bin_icon} className='hidden group-hover:block w-4 cursor-pointer not-dark:invert' alt="" />
+
+                </div>    
+
+             ))
+        }
       </div>
     </div>
   )
